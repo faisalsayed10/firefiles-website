ada99e4bd0e8194db15e201c587db6655589206d
faisalsayed10 <faisal.sayed502@gmail.com>
2022-05-15 16:20:39 +0530

    Redesigned Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,60 +1,131 @@
-import React from "react";
+import { BrandDiscord, BrandGithub, BrandTwitter, Coin } from "tabler-icons-react";
 
 const Footer = () => {
 	return (
-		<footer className="text-gray-600 body-font border-t">
-			<div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-				<a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-					<img src="/firefiles-logo.png" alt="Firefiles" className="w-10 h-10 mr-2" />
-					<span className="ml-3 text-xl">Firefiles</span>
-				</a>
-				<p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-					© {new Date().getFullYear()} Firefiles —
-					<a
-						href="https://twitter.com/faisal_sayed05"
-						className="text-gray-600 ml-1"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						@faisal_sayed05
+		<footer className="text-gray-400 bg-gray-900 body-font relative z-10">
+			<div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+				<div className="w-64 flex-sh`	rink-0 md:mx-0 mx-auto text-center md:text-left">
+					<a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+						<img src="/firefiles-logo.png" className="w-10 h-10" />
+						<span className="ml-3 text-xl">Firefiles</span>
 					</a>
-				</p>
-				<span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-					<a
-						href="https://github.com/faisalsayed10/firefiles"
-						className="text-gray-500 cursor-pointer"
-					>
-						<svg
-							fill="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							className="w-6 h-6"
-							viewBox="0 0 24 24"
+					<p className="mt-2 text-sm text-gray-500">© Copyright 2022 Faisal Sayed</p>
+					<span className="inline-flex mt-5 justify-center sm:justify-start">
+						<a className="text-gray-400" href="https://twitter.com/tryfirefiles">
+							<BrandTwitter />
+						</a>
+						<a className="ml-3 text-gray-400" href="https://discord.com">
+							<BrandDiscord />
+						</a>
+						<a className="ml-3 text-gray-400" href="https://github.com/faisalsayed10/firefiles">
+							<BrandGithub />
+						</a>
+						<a
+							className="ml-3 text-gray-400"
+							href="https://opencollective.com/faisalsayed10/projects/firefiles"
 						>
-							<path
-								fillRule="evenodd"
-								clipRule="evenodd"
-								d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"
-							></path>
-						</svg>
-					</a>
-					<a
-						href="https://twitter.com/faisal_sayed05"
-						className="text-gray-500 cursor-pointer ml-3"
-					>
-						<svg
-							fill="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							className="w-6 h-6"
-							viewBox="0 0 24 24"
-						>
-							<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-						</svg>
-					</a>
-				</span>
+							<Coin />
+						</a>
+					</span>
+				</div>
+				<div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+					<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+						<h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+							ABOUT
+						</h2>
+						<nav className="list-none mb-10">
+							<li>
+								<a
+									className="text-gray-400 hover:text-white"
+									href="https://github.com/faisalsayed10"
+								>
+									Team
+								</a>
+							</li>
+							<li>
+								<a className="text-gray-400 hover:text-white" href="/faq">
+									FAQ
+								</a>
+							</li>
+							<li>
+								<a
+									className="text-gray-400 hover:text-white"
+									href="https://github.com/faisalsayed10/firefiles/releases"
+								>
+									Changelog
+								</a>
+							</li>
+						</nav>
+					</div>
+					<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+						<h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+							DOWNLOADS
+						</h2>
+						<nav className="list-none mb-10">
+							<li>
+								<a className="text-gray-700">MacOS</a>
+							</li>
+							<li>
+								<a className="text-gray-700">Windows</a>
+							</li>
+							<li>
+								<a className="text-gray-700">Linux</a>
+							</li>
+						</nav>
+					</div>
+					<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+						<h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
+							DEVELOPERS
+						</h2>
+						<nav className="list-none mb-10">
+							<li>
+								<a className="text-gray-400 hover:text-white" href="/docs">
+									Documentation
+								</a>
+							</li>
+							<li>
+								<a
+									className="text-gray-400 hover:text-white"
+									href="https://github.com/faisalsayed10/firefiles/blob/main/docs/CONTRIBUTING.md"
+								>
+									Contribute
+								</a>
+							</li>
+							<li>
+								<a className="text-gray-400 hover:text-white" href="/docs/self-hosted/01-setup">
+									Self Host
+								</a>
+							</li>
+						</nav>
+					</div>
+					<div className="lg:w-1/4 md:w-1/2 w-full px-4">
+						<h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">ORG</h2>
+						<nav className="list-none mb-10">
+							<li>
+								<a
+									className="text-gray-400 hover:text-white"
+									href="https://opencollective.com/faisalsayed10/projects/firefiles"
+								>
+									Sponsor Us
+								</a>
+							</li>
+							<li>
+								<a
+									className="text-gray-400 hover:text-white"
+									href="https://github.com/faisalsayed10/firefiles/blob/main/LICENSE"
+								>
+									License
+								</a>
+							</li>
+							<li>
+								<a className="text-gray-700">Privacy</a>
+							</li>
+							<li>
+								<a className="text-gray-700">Terms</a>
+							</li>
+						</nav>
+					</div>
+				</div>
 			</div>
 		</footer>
 	);
