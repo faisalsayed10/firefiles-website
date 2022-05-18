4541a271287e7f973e207622bafe2d77f48adf2b
faisalsayed10 <faisal.sayed502@gmail.com>
2022-05-18 15:20:40 +0530

    Added feature highlights

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from "next/head";
 import { useEffect } from "react";
-import About from "../components/About";
+import CallToAction from "../components/CallToAction";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import Highlights from "../components/Highlights";
 import Navbar from "../components/Navbar";
 import Pricing from "../components/Pricing";
-import UseCases from "../components/UseCases";
+import Providers from "../components/Providers";
 
 export default function Home() {
 	useEffect(() => {
@@ -17,16 +18,17 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="main">
+		<div className="main bg-black text-white">
 			<Head>
-				<title>✨ Firefiles ✨</title>
+				<title>The Open Source Dropbox Alternative | Firefiles</title>
 			</Head>
 			<Navbar />
 			<Hero />
-			<About />
+			<Providers />
 			<Features />
-			<UseCases />
+			<Highlights />
 			<Pricing />
+			<CallToAction />
 			<Footer />
 		</div>
 	);
